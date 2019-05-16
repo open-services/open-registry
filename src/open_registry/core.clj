880ffeda853f7856b9d7de4c8befb65360451383
victorbjelkholm <victorbjelkholm@gmail.com>
2019-05-16 22:08:19 +0200

    Check for updates on boot if UPDATE_ON_BOOT is set
    Changed using streaming to polling for npm-registry-follow
    
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- src/open_registry/core.clj
+++ src/open_registry/core.clj
@@ -1,8 +1,8 @@
 (ns open-registry.core
   (:require [clojure.pprint :refer [pprint]]
             [clojure.java.io :as io]
-            [ipfs-api.files :refer [write read path-exists?]]
-            [npm-registry-follow.core :refer [listen-for-changes]]
+            [ipfs-api.files :refer [write read path-exists? ls]]
+            [npm-registry-follow.core :refer [poll-for-changes]]
             [open-registry.http :as http]
             [open-registry.metrics :as metrics])
   (:gen-class))
@@ -10,6 +10,7 @@
 (defn handle-change [package-name]
   (let [path (format "/npmjs.org/%s/metadata.json" package-name)
         exists? (path-exists? http/api-multiaddr path)]
+    (println (str "[received update] " package-name))
     (when exists?
       ;; TODO seems sometimes npm are not up-to-date with their own registry
       ;; as too quick requests can give us old information, even though the
@@ -23,15 +24,57 @@
 ;; packages that already exists in our cache
 (defn update-metadata-for-existing-packages []
   (println "Now listening for npm registry changes")
-  (listen-for-changes handle-change))
+  (poll-for-changes handle-change))
+
+(defn is-scoped? [pkg-name]
+  (= (get pkg-name 0) \@))
+
+;; Gets all currently installed packages
+(defn get-all-installed-packages
+  ([]
+   (get-all-installed-packages "/npmjs.org"))
+  ([directory]
+   (let [packages (ls http/api-multiaddr directory)
+         to-return (atom [])]
+     (doseq [package packages
+             :let [pkg-name (:name package)
+                   full-path (str directory "/" pkg-name)]]
+       (if (is-scoped? pkg-name)
+         (let [pkgs (get-all-installed-packages full-path)]
+           (doseq [pkg pkgs]
+             (swap! to-return conj (str pkg-name "/" pkg))))
+         (swap! to-return conj pkg-name)))
+     @to-return)))
+
+;; runs through ALL currently installed packages and forces an update
+(defn update-all-packages []
+  (let [pkgs (get-all-installed-packages)
+        updated (atom 0)
+        t (count pkgs)]
+    (add-watch updated nil (fn [_ _ _ _]
+                             (println (format "[boot-update] %s/%s" @updated t))))
+    (doall (pmap (fn [pkg]
+                   (do
+                     (http/metadata-handler pkg true)
+                     (swap! updated inc)
+                     nil))
+                 pkgs))))
+
+(comment
+  (get-all-installed-packages)
+  (update-all-packages))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "5432"))
         metrics-port (Integer/parseInt (or (System/getenv "METRICS_PORT") "2345"))
         threads (Integer/parseInt (or (System/getenv "SERVER_THREADS") "128"))
-        in-dev? (Boolean/parseBoolean (or (System/getenv "SERVER_DEV") "false"))]
+        in-dev? (Boolean/parseBoolean (or (System/getenv "SERVER_DEV") "false"))
+        update-on-boot? (Boolean/parseBoolean (or (System/getenv "UPDATE_ON_BOOT") "false"))]
+    ;; TODO should schedule this once a day or something, to make sure we don't
+    ;; miss updates from the polling/streaming
+    (if update-on-boot?
+      (future (update-all-packages)))
     (update-metadata-for-existing-packages)
     (http/start-server port threads in-dev?)
     (metrics/start-server metrics-port)
